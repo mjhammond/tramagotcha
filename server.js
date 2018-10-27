2fc8f8bf182d21ac54f4dc43a4f159b476ae2345
Matthew Hammond <matthew.hammond@laterooms.com>
2018-10-27 14:43:25 +0100

    chore: initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const Hapi = require('hapi');
+
+const server = Hapi.server({
+    port: 3000,
+    host: 'localhost'
+});
+
+const init = async () => {
+
+    await server.start();
+    console.log(`Server running at: ${server.info.uri}`);
+};
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+
+        return 'Tramagotcha!';
+    }
+});
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
