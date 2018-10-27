b8631d34de8efc1a33e652f4243da18bc61dfd3e
Matthew Hammond <matthew.hammond@laterooms.com>
2018-10-27 16:54:44 +0100

    style: add login styles

--- pages/Login/index.js
+++ pages/Login/index.js
@@ -2,12 +2,23 @@ import React from "react";
 import { Route } from "react-router";
 import Input from "@material-ui/core/Input";
 import Card from "@material-ui/core/Card";
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+    root: {
+        flexGrow: 1,
+        maxWidth: "100%",
+        padding: theme.spacing.unit * 2,
+    },
+});
 
 class Login extends React.Component {
   state = {
     username: "",
     password: ""
   };
+
 
   handleInput = name => event => {
     this.setState({
@@ -16,20 +27,43 @@ class Login extends React.Component {
   };
 
   render() {
+    const classes = this.props.classes;
     return (
+    <Grid
+        className={classes.root}
+        container
+        spacing={16}
+        direction="row"
+        justify="center"
+        alignItems="center"
+    >
       <form className="" noValidate autoComplete="off">
-        <Input
-          id="standard-name"
-          name="username"
-          value={this.state.username}
-          className="username"
-          placeholder="username"
-          disableUnderline
-          onChange={this.handleInput("username")}
-        />
+        <Grid item xs={12}>
+            <Input
+            id="standard-name"
+            name="username"
+            value={this.state.username}
+            className="username"
+            placeholder="username"
+            disableUnderline
+            onChange={this.handleInput("username")}
+            />
+        </Grid>
+        <Grid item xs={12}>
+            <Input
+            id="standard-name"
+            name="password"
+            value={this.state.password}
+            className="password"
+            placeholder="password"
+            disableUnderline
+            onChange={this.handleInput("password")}
+            />
+        </Grid>
       </form>
+    </Grid>
     );
   }
 }
 
-export default Login;
+export default withStyles(styles)(Login);
