9d5e6becba66a40f64ed48a0e92a6eeca613734e
Matthew Hammond <matthew.hammond@laterooms.com>
2018-10-28 09:56:59 +0000

    fix: get items from api

--- src/Components/Items/index.js
+++ src/Components/Items/index.js
@@ -1,17 +1,39 @@
-import React from "react";
-import "./items.css";
+import React, { Component } from 'react';
+import axios from 'axios';
+import './items.css';
 
-const ItemList = ({ className, items, closeItemDrawer }) => (
-  <div className={`items ${className}`}>
-    <span className="bobsBurgers" onClick={event => closeItemDrawer(event)}>
-      <i className="fas fa-bars" />
-    </span>
-    {items.map((item, i) => (
-      <span key={i} className="item">
-        {item.itemName}
-      </span>
-    ))}
-  </div>
-);
+class ItemList extends Component {
+    state = {
+        items: []
+    }
+
+    render() {
+        return (
+            <div className={`items ${this.props.className}`}>
+                <span className="bobsBurgers" onClick={event => this.props.closeItemDrawer(event)}>
+                    <i className="fas fa-bars" />
+                </span>
+                {this.props.items.map((item, i) => (
+                    <span key={i} className="item">
+                        {item.Name}
+                    </span>
+                ))}
+            </div>
+        );
+    }
+}
+
+// const ItemList = ({ className, items, closeItemDrawer }) => (
+//   <div className={`items ${className}`}>
+//     <span className="bobsBurgers" onClick={event => closeItemDrawer(event)}>
+//       <i className="fas fa-bars" />
+//     </span>
+//     {items.map((item, i) => (
+//       <span key={i} className="item">
+//         {item.itemName}
+//       </span>
+//     ))}
+//   </div>
+// );
 
 export default ItemList;
--- src/Components/UserInfo/index.js
+++ src/Components/UserInfo/index.js
@@ -1,57 +1,78 @@
-import React from "react";
-import "./userinfo.css";
-import ItemList from "../Items";
+import React from 'react';
+import axios from 'axios';
+import './userinfo.css';
+import ItemList from '../Items';
 
 class UserInfo extends React.Component {
-  state = {
-    itemDrawerOpen: false
-  };
+    state = {
+        itemDrawerOpen: false,
+        items: [],
+    };
 
-  itemButtonClickHandler = e => {
-    const { itemDrawerOpen } = this.state;
-    e.preventDefault();
-    this.setState({ itemDrawerOpen: !itemDrawerOpen });
-  };
+    itemButtonClickHandler = e => {
+        const { itemDrawerOpen } = this.state;
+        e.preventDefault();
+        this.setState({ itemDrawerOpen: !itemDrawerOpen });
+        this.getItems();
+    };
 
-  closeItemDrawer = e => {
-    e.preventDefault();
-    this.setState({ itemDrawerOpen: false });
-  };
+    closeItemDrawer = e => {
+        e.preventDefault();
+        this.setState({ itemDrawerOpen: false });
+    };
 
-  render() {
-    const { itemDrawerOpen } = this.state;
-    const { userInfo, items } = this.props;
-    return (
-      <div className="userInfo">
-        <span className="petName"> {userInfo.petName}</span>
-        <section className="columns is-mobile is-gapless" id="userInfo">
-          <section className="column">
-            <div className="userData">
-                <span className="userInfoSpan" id="level">
-                Level: {userInfo.currentLevel}
+    getItems() {
+        axios
+            .get(`http://localhost:6006/getItems?ID=${this.props.userInfo.ID}`)
+            .then(res => {
+                console.log(res);
+
+                this.setState(() => ({
+                    items: res.data,
+                }));
+
+                return res;
+            })
+            //   .then(json => console.log(JSON.stringify(json)))
+            .catch(console.error);
+    }
+
+    render() {
+        const { itemDrawerOpen } = this.state;
+        const { userInfo, items } = this.props;
+        console.log(userInfo);
+        return (
+            <div className="userInfo">
+                <span className="petName"> {userInfo.petName}</span>
+                <section className="columns is-mobile is-gapless" id="userInfo">
+                    <section className="column">
+                        <div className="userData">
+                            <span className="userInfoSpan" id="level">
+                                Level: {userInfo.currentLevel}
+                            </span>
+                            <span className="userInfoSpan" id="currency">
+                                Currency: {userInfo.currentCurrency}
+                            </span>
+                            <span className="userInfoSpan" id="xp">
+                                Xp: {userInfo.currentXp}
+                            </span>
+                        </div>
+                    </section>
+                </section>
+                <span
+                    className="button is-primary is-hovered is-inverted"
+                    onClick={event => this.itemButtonClickHandler(event)}
+                >
+                    Items
                 </span>
-                <span className="userInfoSpan" id="currency">
-                Currency: {userInfo.currentCurrency}
-                </span>
-                <span className="userInfoSpan" id="xp">
-                Xp: {userInfo.currentXp}
-                </span>
+                <ItemList
+                    userID={userInfo.ID}
+                    items={this.state.items}
+                    className={itemDrawerOpen ? 'open' : 'closed'}
+                    closeItemDrawer={this.closeItemDrawer}
+                />
             </div>
-          </section>
-        </section>
-        <span
-          className="button is-primary is-hovered is-inverted"
-          onClick={event => this.itemButtonClickHandler(event)}
-        >
-          Items
-        </span>
-        <ItemList
-          items={items}
-          className={itemDrawerOpen ? "open" : "closed"}
-          closeItemDrawer={this.closeItemDrawer}
-        />
-      </div>
-    );
-  }
+        );
+    }
 }
 export default UserInfo;
