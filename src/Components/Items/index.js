0163343dd1769aeadbf8d2ef2fef90f3fe869a76
Will Barker <willbarkerwork@gmail.com>
2018-10-28 02:43:24 +0000

    item drawer

--- src/Components/Items/index.js
+++ src/Components/Items/index.js
@@ -1,8 +1,11 @@
 import React from "react";
 import "./items.css";
 
-const ItemList = ({ className, items }) => (
+const ItemList = ({ className, items, closeItemDrawer }) => (
   <div className={`items ${className}`}>
+    <span className="bobsBurgers" onClick={event => closeItemDrawer(event)}>
+      <i className="fas fa-bars" />
+    </span>
     {items.map((item, i) => (
       <span key={i} className="item">
         {item.itemName}
